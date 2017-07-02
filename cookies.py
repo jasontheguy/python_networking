dd03f5e87c050b7bae7c0dd188c80eee9c857122
Jason Renaud <jjrenaud@protonmail.com>
2017-07-02 16:44:30 -0400

    Adding

--- cookies.py
+++ cookies.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 from http.cookiejar import CookieJar
-cook = CookieJar()
+import datetime
+cookie_jar = CookieJar()
 from urllib.request import *
-opener = build_opener(HTTPCookieProcessor(cook))
+opener = build_opener(HTTPCookieProcessor(cookie_jar))
 opener.open('http://www.github.com')
-print(len(cook))
+print(len(cookie_jar))
+print(cookie_jar)
+cookies = list(cookie_jar)
+print(cookies[0].name)
+print(cookies[0].expires)
+print(datetime.datetime.fromtimestamp(cookies[0].expires))
