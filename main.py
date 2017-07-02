aa81981ebca7b929b936bcba57ea01ae3ba010ba
Jason Renaud <jjrenaud@protonmail.com>
2017-07-02 14:34:53 -0400

    OK

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-import sys
-import urllib.request
+import sys, urllib.request
+
 
 try:
     rfc_num = int(sys.argv[1])
-    except(IndexError,ValueError):
+except (IndexError,ValueError):
         print('Must supply valid RFC number')
         sys.exit(2)
-            template = 'http://www.ietf.org/rfc/rfc{}.txt'
-            url = template.format(rfc_num)
-            rfc_raw = urllib.request.urlopen{url}.read()
-            rfc = rfc_raw.decode()
-            print(rfc)
+template = 'http://www.ietf.org/rfc/rfc{}.txt'
+url = template.format(rfc_num)
+rfc_raw = urllib.request.urlopen{url}.read()
+rfc = rfc_raw.decode()
+print(rfc)
 
             
